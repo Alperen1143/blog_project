f6975ebddf7280f901aa311c984292fb58cd0005
Baver-Tekin <bavertekin21@icloud.com>
2026-05-13 10:56:09 +0300

    I added a category section to the project.

--- anasayfa/admin.py
+++ anasayfa/admin.py
@@ -6,3 +6,12 @@ from .models import BlogPost
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ("title", "created_at")
     search_fields = ("title", "content")
+    
+from django.contrib import admin
+from .models import Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "is_active", "created_at")
+    search_fields = ("name", "slug")
+    list_filter = ("is_active", "created_at")
